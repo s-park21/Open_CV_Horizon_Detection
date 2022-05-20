74b506feeae720fd344173acbd7474d719b95581
Angus McLennan <amcl0006@student.monash.edu>
2022-05-20 21:10:11 +1000

    asd

--- Test_Image.cpp
+++ Test_Image.cpp
@@ -8,6 +8,31 @@ using namespace std;
  int main( int argc, char** argv )
  {
     VideoCapture cap(0); //capture the video from webcam
-    
+
+  if (!cap.isOpened()) {
+    cerr << "ERROR: Unable to open the camera" << endl;
+    return 0;
+  }
+
+  Mat frame;
+  cout << "Start grabbing, press a key on Live window to terminate" << endl;
+  while(1) {
+    cap >> frame;
+    if (frame.empty()) {
+        cerr << "ERROR: Unable to grab from the camera" << endl;
+        break;
+    }
+    imshow("Live",frame);
+    int key = cv::waitKey(20);
+    key = (key==255) ? -1 : key;
+    if (key>=0)
+      break;
+  }
+
+  cout << "Closing the camera" << endl;
+  cap.release();
+  destroyAllWindows();
+  cout << "bye!" <<endl;
+  return 0;
     return 0;
  }
