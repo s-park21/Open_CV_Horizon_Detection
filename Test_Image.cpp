67eb5399b8db0c02c4946e7e4b6971dae13c76a3
Angus McLennan <amcl0006@student.monash.edu>
2022-05-20 20:49:15 +1000

    test

---
+++ Test_Image.cpp
@@ -0,0 +1,13 @@
+#include <iostream>
+#include "opencv2/highgui/highgui.hpp"
+#include "opencv2/imgproc/imgproc.hpp"
+
+using namespace cv;
+using namespace std;
+
+ int main( int argc, char** argv )
+ {
+    VideoCapture cap(0); //capture the video from webcam
+    
+    return 0;
+ }
